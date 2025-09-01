b3b32be86e9ad4cdc861830232f5ada9a70491d4
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-09-01 00:56:22 -0300

    feat: agregar endpoint raíz (/) en auth, eventos, puntos y ranking

---
+++ puntos-service/app.py
@@ -0,0 +1,13 @@
+# Iniciamos codigo base p/ levantar servidor
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+PUERTO_PUNTOS = 8003
+
+# Endpoint raíz para verificar que el servicio está corriendo correctamente
+@app.route("/")
+def inicio():
+    return jsonify({"status":"ok"}), 200
+
+if __name__ == "__main__":
+    app.run(debug=True, port=PUERTO_PUNTOS)

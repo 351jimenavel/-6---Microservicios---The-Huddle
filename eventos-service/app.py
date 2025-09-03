5ce5cc0a50d0cbe7b830973ff8aaed37bccb2a6c
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-09-03 12:58:30 -0300

    Agregar logica de metodo GET para listar eventos y realizar filtros

--- eventos-service/app.py
+++ eventos-service/app.py
@@ -28,40 +28,74 @@ def inicio():
         "service":"eventos",
         "status":"ok"}), 200
 
-# Endpoint para crear eventos (POST), listar eventos (GET) o detallar un evento (GET + id del evento)
-@app.route("/eventos", methods=["POST"])
+# Endpoint para crear eventos (POST), listar eventos (GET) o realizar filtros (GET)
+@app.route("/eventos", methods=["GET","POST"])
 def crear_evento():
-    
-    # Validacion de autenticacion
-    error = validar_token()
-    if error:
-        return error
-    
-    # Si el token es valido
-    data = request.get_json(silent=True)
-    if data is None:
-        return jsonify({"error":"json inválido"}), 400      # Bad Request
-    
-    nombre_evento = data.get("nombre")
-    puntos_base = data.get("puntos_base")
-    fecha = data.get("fecha")
 
-    if not nombre_evento or not isinstance(puntos_base, int):
-        return jsonify({"error": "faltan campos o tipos inválidos"}), 422
-    
-    print("Nuevo evento creado")
-    try:
+    ## logica METODO GET
+    if request.method == "GET":
+        args = request.args
+        params = []
+
+        activo_string = args.get("activo")
+        query = "SELECT * FROM eventos WHERE 1=1"
+        ## Filtro opcional (por evento activo o no activo (1 o 0))
+        if activo_string and (activo_string == "0" or activo_string == "1"):
+            activo = int(activo_string)
+            query += " AND activo = ?"
+            params.append(activo)
+        else:
+            return jsonify({"error":"activo debe ser 0 o 1"}), 422
+        
+        query += " ORDER BY id DESC"
+
         with sqlite3.connect(DB_PATH) as conn:
-            cur = conn.execute(
-                "INSERT INTO eventos (nombre, puntos_base, fecha) VALUES (?,?,?)",
-                (nombre_evento, puntos_base, fecha)
-            )
-            evento_id = cur.lastrowid
-    except Exception as e:
-        print("DB ERROR:", repr(e))
-        return jsonify({"error": "db error"}), 500
-    return jsonify({"id":evento_id,"evento": nombre_evento, "puntos": puntos_base, "fecha":fecha, "activo": 1}), 201
+            conn.row_factory = sqlite3.Row       # row_factory permite convertir filas a dict fácilmente.
+            cur  = conn.execute(query, params)
+            rows = cur.fetchall()
 
+        # Serializamos filas a lista de dicts apta para jsonify
+        response = []
+        for r in rows:
+            response.append(dict(r))
+
+        return jsonify({"data":response}), 200
+
+    ## logica METODO POST
+    if request.method == "POST":
+
+        # Validacion de autenticacion
+        error = validar_token()
+        if error:
+            return error
+        
+        # Si el token es valido
+        data = request.get_json(silent=True)
+        if data is None:
+            return jsonify({"error":"json inválido"}), 400      # Bad Request
+        
+        nombre_evento = data.get("nombre")
+        puntos_base = data.get("puntos_base")
+        fecha = data.get("fecha")
+
+        if not nombre_evento or not isinstance(puntos_base, int):
+            return jsonify({"error": "faltan campos o tipos inválidos"}), 422   # Entidad no procesable
+        
+        print("Nuevo evento creado")
+        try:
+            with sqlite3.connect(DB_PATH) as conn:
+                cur = conn.execute(
+                    "INSERT INTO eventos (nombre, puntos_base, fecha) VALUES (?,?,?)",
+                    (nombre_evento, puntos_base, fecha)
+                )
+                evento_id = cur.lastrowid
+        except Exception as e:
+            print("DB ERROR:", repr(e))
+            return jsonify({"error": "db error"}), 500
+        
+        return jsonify({"id":evento_id,"evento": nombre_evento, "puntos": puntos_base, "fecha":fecha, "activo": 1}), 201
+
+# Endpoint para devolver detalles de un evento especifico (GET + id de un evento activo)
 @app.route("/eventos/<int:evento_id>", methods=["GET"])
 def detalle_evento(evento_id):
 
@@ -83,10 +117,5 @@ def detalle_evento(evento_id):
     
     return jsonify(row), 200
         
-        #return jsonify({"data": rows}), 200
-    #return jsonify({id, "nombre", "puntos_base", "fecha", activo}), 200
-    # 1- devolver lista de eventos que existen
-    # 2- devolver detalle de un evento especifico
-
 if __name__ == "__main__":
     app.run(debug=True, port=PUERTO_EVENTOS)
